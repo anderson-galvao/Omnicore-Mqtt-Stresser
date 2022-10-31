e959da6bee4194876037563b6bafc8549aaff949
shehzad-kore <sibrahim@korewireless.com>
2022-10-31 15:22:33 +0530

    Add Local Stresser

---
+++ localstresser/dbfill/fill.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "user=postgres dbname=iot_core_dev password=iot-core-dev host=104.199.209.231 sslmode=disable"
+
+	subscription := "KoreWireless"
+	device := "Stresser"
+	registry := "KoreWireless"
+	entries := 100
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	sqlStatement := `INSERT INTO public."Device" (
+	subscription , parent, devicename, fullname, loglevel, blocked, numid, registry, capresent, createdon,updatedon, metadata, credentials) VALUES (
+		$1::character varying, $2::character varying, $3::character varying, $4::character varying, 'info'::character varying, false::boolean, $5, $6::character varying, true::boolean, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP, '{}'::json, '[
+				{
+				"id":"1",
+				"publicKey": {
+					"format": "RSA_X509_PEM",
+					"key": "-----BEGIN CERTIFICATE-----\nMIIDujCCAqKgAwIBAgITaOszYpBme+SRHZUkFWLgDs7EMDANBgkqhkiG9w0BAQsF\nADAeMQ0wCwYDVQQKEwRrb3JlMQ0wCwYDVQQDEwRrb3JlMB4XDTIyMDgwNDEwMzQy\nMloXDTMyMDgwMTEwMzIwOFowADCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC\nggEBAMFvTBHPdgH7+5wVlUnEdIS/0a4p9fkVzMdEMdDVr5s62VoGO7nZWxMCaxxU\nXqQiGuX3N7SINyD7h8LI8CxQsn5zyDda3QVNGU7I96iWjwzOYJmNHAN1nRI2hRDY\n8fJoQgTZI+IiRDBmgkmL9yjTY04qY7UP8zpofuMnKRuTwP6Ey1eFEMBqFfvgwrVl\niLNcq9At0bd/vlQ0VUnKV6oKqSTq9ZDPB6Cxu5amhejVwTeE6p5GGmiKw5vskmtB\ndGNgsom1K/pJdOMes8lODVp00tIVnsplL3jLgrWfbfCPALRnGz/C5XlKW8fNKEuW\nqFw2Lhnk51dtobw/oBo7vJcx2w0CAwEAAaOCAQ0wggEJMA4GA1UdDwEB/wQEAwIF\noDAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBQUiRS8X3OWpJNpjQoJ+22xGb3xXTAf\nBgNVHSMEGDAWgBTKO7S10CConGVgZZli7NVAcim/AzCBjQYIKwYBBQUHAQEEgYAw\nfjB8BggrBgEFBQcwAoZwaHR0cDovL3ByaXZhdGVjYS1jb250ZW50LTYyZTM5YmRh\nLTAwMDAtMjI0My1hNjFhLTNjMjg2ZDRlZWUwYS5zdG9yYWdlLmdvb2dsZWFwaXMu\nY29tL2MwZGQxZjg3ZDcwZGZhMDEwNGEwL2NhLmNydDAZBgNVHREBAf8EDzANggtn\nYWRnZW9uLmNvbTANBgkqhkiG9w0BAQsFAAOCAQEAK82b/xGn8B6Nfogw0myKjy3O\nWg53YPXuct3E04qRmD3JJtFzpSkjI2WyRmIkRX1b5SKF+ImOmGzvENZDkjT/Y2I/\nnsBL639OlXnz/+GYSq4rL6fVxXistP4LGA+khoBYSfHFZb7EYoVOYJFzZjnvJbtz\n7XG0jTMeHo8KhCBPxrNWkOERrcc7OWqREldQ36yg7zdbRLjDOjeD6FByTrpRhbDC\n0AeozF9ug9W/gPYtnkI++ksUqjJcV06uGd+9XLJPGcjH0Bai1alxROh+dkWx6TcB\nHC94el4KR6EJijMvylmnOyKHedmYaDvb52+B6zXTW9rQkh1UycmONlmAlA3OeQ==\n-----END CERTIFICATE-----\n"
+				}
+				}
+		
+			]'::json) ON CONFLICT DO NOTHING`
+
+	for i := 0; i < entries; i++ {
+		d3 := fmt.Sprintf("subscriptions/%s/registries/%s/devices", subscription, registry)
+		devId := fmt.Sprintf("%s%d", device, i)
+		d5 := fmt.Sprintf("%s/%s", d3, devId)
+		_, err = db.Exec(sqlStatement, subscription, d3, devId, d5, i+500, registry)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
---
+++ localstresser/iot-gw-stresser/worker.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messageId uint64
+
+type PayloadGenerator func(i int) string
+
+func defaultPayloadGen() PayloadGenerator {
+	return func(i int) string {
+		return fmt.Sprintf("this is msg #%d!", i)
+	}
+}
+
+func constantPayloadGenerator(payload string) PayloadGenerator {
+	return func(i int) string {
+		return payload
+	}
+}
+
+func filePayloadGenerator(filepath string) PayloadGenerator {
+	inputPath := strings.Replace(filepath, "@", "", 1)
+	content, err := ioutil.ReadFile(inputPath)
+	if err != nil {
+		fmt.Printf("error reading payload file: %v\n", err)
+		os.Exit(1)
+	}
+	return func(i int) string {
+		return string(content)
+	}
+}
+
+type Worker struct {
+	WorkerId             int
+	BrokerUrl            string
+	Username             string
+	Password             string
+	SkipTLSVerification  bool
+	NumberOfMessages     int
+	PayloadGenerator     PayloadGenerator
+	Timeout              time.Duration
+	Retained             bool
+	PublisherQoS         byte
+	SubscriberQoS        byte
+	CA                   []byte
+	Cert                 []byte
+	Key                  []byte
+	PauseBetweenMessages time.Duration
+}
+
+func setSkipTLS(o *mqtt.ClientOptions) {
+	oldTLSCfg := o.TLSConfig
+	if oldTLSCfg == nil {
+		oldTLSCfg = &tls.Config{}
+	}
+	oldTLSCfg.InsecureSkipVerify = true
+	o.SetTLSConfig(oldTLSCfg)
+}
+
+func NewTLSConfig(ca, certificate, privkey []byte) (*tls.Config, error) {
+	// Import trusted certificates from CA
+	certpool := x509.NewCertPool()
+	ok := certpool.AppendCertsFromPEM(ca)
+
+	if !ok {
+		return nil, fmt.Errorf("CA is invalid")
+	}
+
+	// Import client certificate/key pair
+	cert, err := tls.X509KeyPair(certificate, privkey)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create tls.Config with desired tls properties
+	return &tls.Config{
+		// RootCAs = certs used to verify server cert.
+		RootCAs: certpool,
+		// ClientAuth = whether to request cert from server.
+		// Since the server is set up for SSL, this happens
+		// anyways.
+		ClientAuth: tls.NoClientCert,
+		// ClientCAs = certs used to validate client cert.
+		ClientCAs: nil,
+		// InsecureSkipVerify = verify that cert contents
+		// match server. IP matches what is in cert etc.
+		InsecureSkipVerify: false,
+		// Certificates = list of certs client sends to server.
+		Certificates: []tls.Certificate{cert},
+	}, nil
+}
+
+func (w *Worker) Run(ctx context.Context) {
+	verboseLogger.Printf("[%d] initializing\n", w.WorkerId)
+
+	queue := make(chan [2]string)
+	cid := w.WorkerId
+	_ = randomSource.Int31()
+
+	_, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	topicName := fmt.Sprintf(topicNameTemplate, w.WorkerId)
+	subscriberClientId := fmt.Sprintf(subscriberClientIdTemplate, w.WorkerId)
+	publisherClientId := fmt.Sprintf(publisherClientIdTemplate, w.WorkerId)
+
+	verboseLogger.Printf("[%d] topic=%s subscriberClientId=%s publisherClientId=%s\n", cid, topicName, subscriberClientId, publisherClientId)
+	password := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJteS1pb3QtMzU2MzA1IiwiZXhwIjoxNjk1NjY3OTc0LCJpYXQiOjE2NjQxMTAzNzR9.T_kzjb2mQVtF_0J9zY7QuJiY8z5sd8-VNN8XW06xo1CGQvpjYnOcfVs0tfh6t8VWDZq5PndcbNTNCybZbJd4Dhzxw_Rz-6PJoFqe9HisIl7xyRNanxzVEeeBE-3SSmJRSPTGYjx6VHZU2xRYCNmXSi0UdLPi6P43-TdK3gPZDR57CJQbbGUdVSotVAz9tbETNBdthZK6tpw8o8EgKpsBfKKOzNmXYAtt9wHuoPSI_HlFSviMMEEYZuC8Ss3xJ6nGWJuQEY6G4epsrnjxneT3fHGcjflI-if4FmdRmxmcvCQBrZd2UGvylJTK96Ir3WQfcJbQdT2n9Fc7VVifYR3Lzw"
+	publisherOptions := mqtt.NewClientOptions().SetClientID(publisherClientId).SetUsername("unused").SetPassword(password).AddBroker(w.BrokerUrl)
+
+	subscriberOptions := mqtt.NewClientOptions().SetClientID(subscriberClientId).SetUsername("unused").SetPassword(password).AddBroker(w.BrokerUrl)
+
+	subscriberOptions.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		queue <- [2]string{msg.Topic(), string(msg.Payload())}
+	})
+
+	if len(w.CA) > 0 || len(w.Key) > 0 {
+		tlsConfig, err := NewTLSConfig(w.CA, w.Cert, w.Key)
+		if err != nil {
+			panic(err)
+		}
+		subscriberOptions.SetTLSConfig(tlsConfig)
+		publisherOptions.SetTLSConfig(tlsConfig)
+	}
+
+	if w.SkipTLSVerification {
+		setSkipTLS(publisherOptions)
+		setSkipTLS(subscriberOptions)
+	}
+
+	// subscriber := mqtt.NewClient(subscriberOptions)
+
+	// verboseLogger.Printf("[%d] connecting subscriber\n", w.WorkerId)
+	// if token := subscriber.Connect(); token.WaitTimeout(w.Timeout) && token.Error() != nil {
+	// 	resultChan <- Result{
+	// 		WorkerId:     w.WorkerId,
+	// 		Event:        ConnectFailedEvent,
+	// 		Error:        true,
+	// 		ErrorMessage: token.Error(),
+	// 	}
+
+	// 	return
+	// }
+
+	// defer func() {
+	// 	verboseLogger.Printf("[%d] unsubscribe\n", w.WorkerId)
+
+	// 	if token := subscriber.Unsubscribe(topicName); token.WaitTimeout(w.Timeout) && token.Error() != nil {
+	// 		fmt.Printf("failed to unsubscribe: %v\n", token.Error())
+	// 	}
+
+	// 	subscriber.Disconnect(5)
+	// }()
+
+	// verboseLogger.Printf("[%d] subscribing to topic\n", w.WorkerId)
+	// if token := subscriber.Subscribe(topicName, w.SubscriberQoS, nil); token.WaitTimeout(w.Timeout) && token.Error() != nil {
+	// 	resultChan <- Result{
+	// 		WorkerId:     w.WorkerId,
+	// 		Event:        SubscribeFailedEvent,
+	// 		Error:        true,
+	// 		ErrorMessage: token.Error(),
+	// 	}
+
+	// 	return
+	// }
+
+	publisher := mqtt.NewClient(publisherOptions)
+	verboseLogger.Printf("[%d] connecting publisher\n", w.WorkerId)
+	if token := publisher.Connect(); token.WaitTimeout(w.Timeout) && token.Error() != nil {
+		resultChan <- Result{
+			WorkerId:     w.WorkerId,
+			Event:        ConnectFailedEvent,
+			Error:        true,
+			ErrorMessage: token.Error(),
+		}
+		return
+	}
+
+	verboseLogger.Printf("[%d] starting control loop %s\n", w.WorkerId, topicName)
+
+	receivedCount := 0
+	publishedCount := 0
+
+	t0 := time.Now()
+	for i := 0; i < w.NumberOfMessages; i++ {
+		text := fmt.Sprintf("{\"id\":%d,\"time\":%d}", messageId, time.Now().UTC().Unix())
+		atomic.AddUint64(&messageId, 1)
+		token := publisher.Publish(topicName, w.PublisherQoS, w.Retained, text)
+		published := token.WaitTimeout(w.Timeout)
+		if published {
+			publishedCount++
+		}
+		time.Sleep(w.PauseBetweenMessages)
+	}
+	publisher.Disconnect(5)
+
+	publishTime := time.Since(t0)
+	verboseLogger.Printf("[%d] all messages published\n", w.WorkerId)
+	resultChan <- Result{
+		WorkerId:          w.WorkerId,
+		Event:             CompletedEvent,
+		PublishTime:       publishTime,
+		ReceiveTime:       time.Since(t0),
+		MessagesReceived:  receivedCount,
+		MessagesPublished: publishedCount,
+	}
+
+	verboseLogger.Printf("[%d] worker finished\n", w.WorkerId)
+}
